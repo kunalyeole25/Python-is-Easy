fba84ea603af368151da7bb1ea2adf29773d9f0f
Kunal Yeole <79986521+kunalyeole25@users.noreply.github.com>
2021-05-16 12:50:01 +0530

    Create Homework1.py

---
+++ Homework1.py
@@ -0,0 +1,23 @@
+"""All Attributes of my favourite song
+My favourite song is BTS's Dynamite, period"""
+#Assigning values to variables
+
+#Strings
+Song_Name = "Dynamite"
+Genre = "Disco Pop"
+SongWriter = "David Stewart & Jessica Agombar"
+Producer = "David Stewart"
+Artists = "BTS"
+
+#Integers
+Year_Released = 2020
+Duration_in_Seconds = 180 + 19
+
+#Printing the variables
+print ("Song Name: ", Song_Name)
+print ("Year Released: ", Year_Released)
+print ("Genre: ", Genre)
+print ("Duration (in seconds): ", Duration_in_Seconds)
+print ("Artists: ", Artists)
+print ("Song Writer: ", SongWriter)
+print ("Producer: ", Producer)
