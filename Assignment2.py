f00730263027fd1abc61d498d9dcb808cb4d2e15
Kunal Yeole <79986521+kunalyeole25@users.noreply.github.com>
2021-05-17 23:09:54 +0530

    Create Assignment2.py

---
+++ Assignment2.py
@@ -0,0 +1,19 @@
+# Defining name of song
+def song_name():
+    Song_Name = "Dynamite"
+    return Song_Name
+
+# Defining released year function
+def year_released():
+    year = 2020
+    return year
+
+# Defining artist function
+def artist():
+    Artist = "BTS"
+    return Artist
+
+# Calling the functions & printing the returned values
+print (song_name())
+print (year_released())
+print (artist())
